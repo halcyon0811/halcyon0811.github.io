1e51fda03d178133f824580f20a2cfa3a5280115
luis <luis.parent@unicaribe.edu.do>
2020-10-13 22:00:28 -0400

    Header for Website

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+      colors: {
+        spacegray: "#333646",
+        darkgray: "#252734",
+        white: "#e9eaeb",
+      },
+        extend: {}
+  },
+  variants: {},
+  plugins: []
+}
