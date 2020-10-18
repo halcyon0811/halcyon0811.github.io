597b0f2e2ee40f73332cb49164dd758c1c8381c7
luis <luis.parent@unicaribe.edu.do>
2020-10-17 23:25:29 -0400

    adding the timeline code

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,13 @@ module.exports = {
         spacegray: "#333646",
         darkgray: "#252734",
         white: "#e9eaeb",
-        aqua: "#64ffda"
+        aqua: "#64ffda",
+        brick: "#b92b27",
+        marine: "#1565C0",
+        graynebula: "#485563",
+        darkslate: "#29323c",
+        purplery: "#6441a5",
+        purpledark: "#2a0845"
       },
         extend: {}
   },
