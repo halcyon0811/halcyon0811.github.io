06a440b23eae9f37ec674dcf2dc787eeedfb20d6
luis <luis.parent@unicaribe.edu.do>
2020-11-01 19:14:09 -0400

    changed the gradient colors to a more darker palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,35 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-    // defaultLineHeights: true,
-    // standardFontWeights: true
-  },
-  purge: [],
-  theme: {
-      colors: {
-        spacegray: "#333646",
-        darkgray: "#252734",
-        white: "#ffffff",
-        aqua: "#64ffda",
-        brick: "#b92b27",
-        marine: "#1565C0",
-        graynebula: "#485563",
-        darkslate: "#29323c",
-        purplery: "#6441a5",
-        purpledark: "#2a0845",
-        whitesmoke: "#eeeeee",
-        almostblack:"#141313",
-        blueish: "#000046",
-        skyblue: "#1CB5E0"
-      },
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+        // defaultLineHeights: true,
+        // standardFontWeights: true
+    },
+    purge: [],
+    theme: {
+        colors: {
+            spacegray: "#333646",
+            darkgray: "#252734",
+            white: "#ffffff",
+            aqua: "#64ffda",
+            brick: "#b92b27",
+            marine: "#1565C0",
+            graynebula: "#485563",
+            darkslate: "#29323c",
+            purplery: "#6441a5",
+            purpledark: "#2a0845",
+            whitesmoke: "#eeeeee",
+            almostblack: "#141313",
+            blueish: "#000046",
+            skyblue: "#1CB5E0",
+            arameo: "#4B79A1",
+            darkblue: "#283E51",
+            mojito1: "#44A08D",
+            mojito2: "#093637"
+
+        },
         extend: {}
-  },
-  variants: {},
-  plugins: []
+    },
+    variants: {},
+    plugins: []
 }
